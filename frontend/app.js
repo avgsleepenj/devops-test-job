1cc6d341c37ec443d5bc3e1432a9c3ba933b11a5
avsspndr <sazanov.andrei1998@gmail.com>
2026-07-10 13:56:10 +0300

    Исправлены ошибки, не позволяющие запустить приложение

--- frontend/app.js
+++ frontend/app.js
@@ -5,13 +5,14 @@ function applyFilters() {
     var breed = $('#filter-breed').val();
     var status = $('#filter-status').val();
 
-    $.getJSON('http://localhost:8888/api/cats', {name: name, age: age, breed: breed, status: status }, function(data) {
+    // Изменил: запрос к бэкэнду через относительный путь вместо прямого запроса на бэкэнд
+    $.getJSON('/api/cats', { name: name, age: age, breed: breed, status: status }, function (data) {
 
         // Очистить текущее содержимое таблицы, кроме заголовка
         $('#pets-table tr:not(:first)').remove();
 
         // Генерация новых строк таблицы
-        var rows = data.map(function(pet) {
+        var rows = data.map(function (pet) {
             return `<tr>
                         <td>${pet.name}</td>
                         <td>${pet.age}</td>
@@ -20,7 +21,7 @@ function applyFilters() {
                         <td>${pet.status}</td>
                     </tr>`;
         }).join('');
-        
+
         // Добавление новых строк в таблицу
         $('#pets-table').append(rows);
     });
